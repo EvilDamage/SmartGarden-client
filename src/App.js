13aeb572a704bd0ab9b1b733cebc8b7fd1e0c911
Przemysław Kuca <evildemage@gmail.com>
2021-11-01 21:17:56 +0100

    Init + routing + bootstrap

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useContext, createContext, useState} from "react";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Redirect, useHistory
+} from "react-router-dom";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import NoMatch from "./pages/NoMatch";
+
+const authContext = createContext();
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <ProvideAuth>
+            <Router>
+                <Switch>
+                    <Route path="/login">
+                        <Login/>
+                    </Route>
+                    <PrivateRoute exact path="/">
+                        <Home/>
+                    </PrivateRoute>
+                    <Route component={NoMatch} />
+                </Switch>
+            </Router>
+        </ProvideAuth>
+    );
+}
+
+function ProvideAuth({children}) {
+    //TODO: add auth
+    const auth = {user: false};
+    return (
+        <authContext.Provider value={auth}>
+            {children}
+        </authContext.Provider>
+    );
+}
+
+function PrivateRoute({children, ...rest}) {
+    let auth = useContext(authContext);
+    return (
+        <Route
+            {...rest}
+            render={({location}) =>
+                auth.user ? (
+                    children
+                ) : (
+                    <Redirect
+                        to={{
+                            pathname: "/login",
+                            state: {from: location}
+                        }}
+                    />
+                )
+            }
+        />
+    );
 }
 
 export default App;
